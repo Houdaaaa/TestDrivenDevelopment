b92538d7d2c7fbba69e37664d413fa1b5ac771e5
Ludovic Merel <ludo.m@live.fr>
2018-10-25 19:43:23 +0200

    Refact ChoiceMystery
    We use tuple to stock the word and sentence. For that, I have added a library =>javatuples

--- program/src/TypeMystery.java
+++ program/src/TypeMystery.java
@@ -1,7 +1,23 @@
 import java.util.*;
+import javafx.util.*;
+import org.json.JSONObject;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
 
 public abstract class TypeMystery {
     public abstract void NextMystery();
 
-    protected abstract Map<String, List<String>> ChoiceMystery(String difficulty);
+    public abstract Pair<String, List<String>> ChoiceMystery(String difficulty);
+
+    public JSONObject ReadDatabase(String path) {
+        try {
+            File file = new File(path);
+            String content = FileUtils.readFileToString(file, "utf-8");
+
+            return new JSONObject(content);
+        }
+        catch (Exception e) { System.out.print(e); }
+
+        return new JSONObject();
+    }
 }
