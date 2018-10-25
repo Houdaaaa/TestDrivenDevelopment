f8dd2b022b846221d2a3eb4c99d3439e10b1e837
Ludovic <ludo.m@live.fr>
2018-10-25 09:14:11 +0200

    Read Json

--- program/src/SentenceMystery.java
+++ program/src/SentenceMystery.java
@@ -1,6 +1,35 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
 public class SentenceMystery extends TypeMystery{
 
     public SentenceMystery() {}
 
-    public void NextMystery() {}
+    public void NextMystery() {
+        try {
+            JSONObject db = ReadDatabase("src/database.json");
+
+            JSONArray easy = db.getJSONArray("easy");
+
+            for (int i=0; i<easy.length(); i++) {
+                JSONObject mystery = easy.getJSONObject(i);
+
+                System.out.println(mystery.get("sentence"));
+            }
+        }
+        catch (Exception e) { System.out.print(e); }
+    }
+
+    private JSONObject ReadDatabase(String path) {
+        try {
+            File file = new File(path);
+            String content = FileUtils.readFileToString(file, "utf-8");
+
+            return new JSONObject(content);
+
+        }
+        catch (Exception e) { System.out.print(e); }
+    }
 }
