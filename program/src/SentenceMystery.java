f146b32f30308ab4ecc0599fe5e341fcd8655ad2
Ludovic <ludo.m@live.fr>
2018-10-26 21:41:30 +0200

    Ludovic (#5)
    * Set up classes
    
    * Read Json
    
    * Delete TestDrivenDevelopment.iml
    
    * Revert "Delete TestDrivenDevelopment.iml"
    
    This reverts commit 97a331a562b11f77fd0f915e258135f365254dd4.
    
    * Add a return in SentenceMystery
    
    * Create the ChoiceMystery
    
    this function is for the sentenceMystery type => research random the mystery for the user
    
    * Refact ChoiceMystery
    
    We use tuple to stock the word and sentence. For that, I have added a library =>javatuples
    
    * Delete main.java
    
    * Update program/src/Main.java
    
    * Create ChoiceLetters in TyeMystery

---
+++ program/src/SentenceMystery.java
@@ -0,0 +1,40 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.*;
+import javafx.util.*;
+
+public class SentenceMystery extends TypeMystery{
+
+    public SentenceMystery() {}
+
+    public void NextMystery() {
+        // TODO: 25-10-18 Set the instance variables in game when game is made
+        Pair<String, List<String>> test = ChoiceMystery("easy");
+        ArrayList<Character> displayLetters = TypeMystery.ChoiceLetters(test.getKey());
+
+        System.out.print(test.getKey());
+        System.out.print(displayLetters);
+    }
+
+    public Pair<String, List<String>> ChoiceMystery(String difficulty) {
+        List statement = new ArrayList();
+        String word = null;
+        Random random = new Random();
+
+        try {
+            JSONObject db = ReadDatabase("src/database.json");
+            JSONArray diff = db.getJSONObject(difficulty).getJSONArray("sentenceMode");
+
+            int randMystery = random.nextInt(diff.length());
+            JSONObject mystery = diff.getJSONObject(randMystery);
+
+            word = (String) mystery.get("word");
+            statement.add(mystery.get("sentence"));
+            statement.add(mystery.get("sentence"));
+
+        }
+        catch (Exception e) { System.out.print(e); }
+
+        return new Pair<String, List<String>>(word, statement);
+    }
+}
