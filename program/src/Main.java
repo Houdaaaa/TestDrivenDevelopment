acb92af25f470cc420af5a56c2fdc97133f98983
Ludovic Merel <ludo.m@live.fr>
2018-11-01 23:07:11 +0100

    Add documentation

--- program/src/Main.java
+++ program/src/Main.java
@@ -3,8 +3,6 @@ import javafx.util.*;
 
 public class Main {
     public static void main(String [] args) {
-        SentenceMystery test = new SentenceMystery();
 
-        test.NextMystery();
     }
 }
