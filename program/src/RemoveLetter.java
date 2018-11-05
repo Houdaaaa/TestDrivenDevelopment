2e921f6a05d958e2e83877d94c630ce9f7da03aa
Ludovic <ludo.m@live.fr>
2018-11-05 14:23:04 +0100

    Fix bugs

--- program/src/RemoveLetter.java
+++ program/src/RemoveLetter.java
@@ -7,10 +7,12 @@ public class RemoveLetter extends TypeBonus {
 
     @Override
     public void ApplyBonus(Game game) {
+        game.GetDifficulty().RetryCoins(game.GetPlayer());
+
         Random random = new Random();
 
         String word = game.GetMystery().getKey();
-        ArrayList<Character> displayLetters = game.GetDisplayLetters();
+        ArrayList<Character> displayLetters = game.GetLetterDisplay();
 
         while (true) {
             int randInt = random.nextInt(displayLetters.size());
@@ -22,7 +24,7 @@ public class RemoveLetter extends TypeBonus {
             }
         }
 
-        used = true;
-        game.SetDisplayLetters(displayLetters);
+        used = false;
+        game.SetLetterDisplay(displayLetters);
     }
 }
