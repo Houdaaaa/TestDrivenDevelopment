56794f5c2c5ede8301ad7a1ea7c1e69515ca0ab9
Houdaaaa <Houda-11.03@hotmail.com>
2018-11-05 13:27:31 +0100

    Add classes Game , Difficulty and Player (#7)
    * Add Game + Difficulty classes
    
    * Merge master branch
    
    * Add Player Class
    
    playerDatabase.json en rouge : à remédier
    
    * Add RetryCoins()/AddCoins() in Difficulty
    
    * Game.Save(), Game.CheckLogin() and Game.Login() Implementation
    
    Détail avec fichier.iml à régler (nom du projet changé)
    
    * Get/Set nameDifficulty
    
    * Update program/.idea/workspace.xml
    
    * Remove .idea
    
    * cached .idea

--- program/src/Main.java
+++ program/src/Main.java
@@ -5,6 +5,19 @@ public class Main {
     public static void main(String [] args) {
         SentenceMystery test = new SentenceMystery();
 
+
+
         test.NextMystery();
+
+
+        Difficulty diff = new Difficulty("easy");
+        TypeMystery typeMystery = test;
+
+        String[] ok = {"one", "two"};
+        Pair<String,String[]> mystery = new Pair<String, String[]>("Hello",ok);
+        Game game = new Game(diff, typeMystery,mystery);
+        game.Login("Houda", "cool");
+        //game.Save();
+
     }
 }
